25709c31baf5e797dcac12893b12207bd94bc6a2
JonatanEnes <jonatan.enes@protonmail.com>
2016-05-22 04:23:47 +0200

    Se implementan los POST y PUT para plantillas, además del POSt para instancias.

--- app/v1/user_endpoints.py
+++ app/v1/user_endpoints.py
@@ -9,8 +9,8 @@ from . import api
 from . import utils as ut
 from .errors import ValidationError
 
-#import registry
-from .configuration_registry import registry
+import registry
+#from .configuration_registry import registry
 
 ENDPOINT = 'http://consul:8500/v1/kv'
 MESOS_FRAMEWORK_ENDPOINT = 'http://mesos_framework.service.int.cesga.es:5000/bigdata/mesos_framework/v1/instance'
@@ -78,6 +78,7 @@ def get_user_instances(username):
         abort(404)
 
 @api.route('/instances/<username>/<service>', methods=['GET'])
+@api.route('/instances/<username>/<service>/', methods=['GET'])
 def get_user_service_instances(username, service):
     app.logger.info('Request for instances of user {} and service {}'.format(username, service))
     try:
@@ -91,6 +92,7 @@ def get_user_service_instances(username, service):
         abort(404)
 
 @api.route('/instances/<username>/<service>/<version>', methods=['GET'])
+@api.route('/instances/<username>/<service>/<version>/', methods=['GET'])
 def get_user_service_version_instances(username, service, version):
     app.logger.info('Request for instances of user {} and service {} with version {}'.format(username, service, version))
     try:
@@ -105,6 +107,7 @@ def get_user_service_version_instances(username, service, version):
 
 
 @api.route('/instances/<username>/<service>/<version>/<instanceid>', methods=['GET'])
+@api.route('/instances/<username>/<service>/<version>/<instanceid>/', methods=['GET'])
 def get_instance(username, service, version, instanceid):
     app.logger.info('Request for instance nodes of user {} and service {} with version {}'.format(username, service, version))
     try:
@@ -118,6 +121,7 @@ def get_instance(username, service, version, instanceid):
         abort(404)
 
 @api.route('/instances/<username>/<service>/<version>/<instanceid>/nodes', methods=['GET'])
+@api.route('/instances/<username>/<service>/<version>/<instanceid>/nodes/', methods=['GET'])
 def get_instance_nodes(username, service, version, instanceid):
     app.logger.info('Request for instance nodes of user {} and service {} with version {}'.format(username, service, version))
     try:
@@ -135,6 +139,7 @@ def get_instance_nodes(username, service, version, instanceid):
         abort(404)
 
 @api.route('/instances/<username>/<service>/<version>/<instanceid>/services', methods=['GET'])
+@api.route('/instances/<username>/<service>/<version>/<instanceid>/services/', methods=['GET'])
 def get_instance_services(username, service, version, instanceid):
     app.logger.info('Request for instance nodes of user {} and service {} with version {}'.format(username, service, version))
     try:
@@ -143,7 +148,7 @@ def get_instance_services(username, service, version, instanceid):
         services = instance.services
         service_names_list = list()
         for service in services:
-            # FIXME Implement a 'toString' methos to describe a service object from the endpoint
+            # FIXME Implement a 'toString' method to describe a service object from the endpoint
             service_names_list.append(service.name)
         return jsonify({'services': service_names_list})
 
@@ -169,75 +174,129 @@ def delete_service():
     #
     # return jsonify({'result': "fail", 'message': "method not implemented"})
 
-
+@api.route('/services', methods=['POST'])
 @api.route('/services/', methods=['POST'])
-def launch_service():
-    username = "jenes"
+def create_service():
 
     # Get data from user parameters
-    data = ut.parse_post_data(request)
+    template_data = ut.parse_post_template_data(request)
 
-    # Log request
-    app.logger.info("User {} wants to launch new cluster with params {}".format(username, str(data)))
+    registry.connect(ENDPOINT)
+    template = registry.register(name=template_data["name"], version=template_data["version"], description="",
+                      template="", options="")
+    return str(template), 200
 
-    # Check that the user, service and flavour are available
-    if not (ut.check_user(username) and ut.check_service(data["service_type"]) and
-            ut.check_flavour(data["service_type"], data["service_name"])):
-        app.logger.info("Can't launch cluster because service doesn't exist or the user is not valid")
-        abort(404)
 
-    # default node attributes data from template
-    instance_template = ut.get_framework_template(
-        TEMPLATE_ENDPOINT + "/{}/{}/v1/instance/".format(data["service_type"], data["service_name"]))
+@api.route('/services/<service>/<version>/<attribute>', methods=['PUT'])
+@api.route('/services/<service>/<version>/<attribute>/', methods=['PUT'])
+def fill_service_by_param(service, version, attribute):
+    if attribute in ("description", "template", "options"):
+        data = request.get_data()
+        registry.connect(ENDPOINT)
+        template = registry.get_service_template(service, version)
+        if attribute == "description":
+            template.description = data
+        elif attribute == "template":
+            template.template = data
+        elif attribute == "options":
+            template.options = data
+    return str(template), 200
 
-    instance_data = ut.get_instance_attributes(instance_template)
-    node_data = instance_template["node"]
-    services_dict = instance_template["services"]
+@api.route('/services/<service>/<version>', methods=['PUT'])
+@api.route('/services/<service>/<version>/', methods=['PUT'])
+def fill_service(service, version):
+    # Get data from user parameters
+    template_data = ut.parse_put_template_data(request)
 
-    # Fill service username
-    data["service_username"] = username
+    template = registry.register(name=service, version=version, description=template_data["description"],
+                      template=template_data["template"], options=template_data["options"])
+    return str(template), 200
 
-    # Instance info
-    instance_data["cpu"] = data["num_nodes"] * data["cpu"]
-    instance_data["mem"] = data["num_nodes"] * data["mem"]
-    instance_data["number_of_disks"] = data["num_nodes"] * data["number_of_disks"]
-    instance_data["number_of_nodes"] = data["num_nodes"]
-    instance_data["instance_name"] = data["clustername"]
-    instance_data["instance_state"] = "registered"
 
-    # Node info
-    node_data["cpu"] = str(data["cpu"])  # You can remove str in the future when registry is fixed
-    node_data["mem"] = str(data["mem"])  # You can remove str in the future when registry is fixed
-    node_data["number_of_disks"] = str(data["number_of_disks"])
-    node_data["status"] = "registered"
 
-    # nodes
-    nodes_dict = dict()
-    name_pattern = node_data["name"]
-    for i in range(int(instance_data["number_of_nodes"])):
-        copied_data = node_data.copy()
-        copied_data["name"] = name_pattern+str(i)
-        copied_data["node_id"] = str(i)
-        copied_data["disks"] = ut.deflate_node_disks(node_data["number_of_disks"], node_data["disk"])
-        copied_data.pop("disk", None)
-        copied_data["networks"] = ut.deflate_node_networks(node_data["network"])
-        copied_data.pop("network", None)
-        nodes_dict[copied_data["name"]] = copied_data
+@api.route('/services/<service>/<version>', methods=['POST'])
+@api.route('/services/<service>/<version>/', methods=['POST'])
+def launch_service(service, version):
+    username = "jenes"
+    options = request.get_json()
+    registry.connect(ENDPOINT)
+    instance = registry.instantiate(username, service, version, options)
+    ut.initialize_networks(instance)
+    return str(instance), 200
 
-    instance_dn, instance_data = ut.register_instance(data, instance_data, nodes_dict, services_dict)
-    app.logger.info("Cluster {} was successfully registered".format(instance_data["service_full_name"]))
 
-    for node in nodes_dict:
-        node_dn = instance_dn + "/nodes/" + nodes_dict[node]["name"]
-        ut.set_node_info(node_dn=node_dn, node_name=nodes_dict[node]["name"],
-                         instance_name=instance_data["service_full_name"])
 
-    data = {"instance_dn": instance_dn}
-    data_json = json.dumps(data)
-    headers = {'Content-type': 'application/json'}
-    response = requests.post(MESOS_FRAMEWORK_ENDPOINT, data=data_json, headers=headers)
-    app.logger.info(
-        "Cluster {} was successfully forwarded to the mesos framework".format(instance_data["service_full_name"]))
-    app.logger.info(
-        "Mesos framework response was {} ".format(response))
-    return jsonify({'result': 'success'})
+
+
+# @api.route('/services/<service>/<version>', methods=['POST'])
+# @api.route('/services/<service>/<version>/', methods=['POST'])
+# def launch_service():
+#     username = "jenes"
+#
+#     # Get data from user parameters
+#     data = ut.parse_post_data(request)
+#
+#     # Log request
+#     app.logger.info("User {} wants to launch new cluster with params {}".format(username, str(data)))
+#
+#     # Check that the user, service and flavour are available
+#     if not (ut.check_user(username) and ut.check_service(data["service_type"]) and
+#             ut.check_flavour(data["service_type"], data["service_name"])):
+#         app.logger.info("Can't launch cluster because service doesn't exist or the user is not valid")
+#         abort(404)
+#
+#     # default node attributes data from template
+#     instance_template = ut.get_framework_template(
+#         TEMPLATE_ENDPOINT + "/{}/{}/v1/instance/".format(data["service_type"], data["service_name"]))
+#
+#     instance_data = ut.get_instance_attributes(instance_template)
+#     node_data = instance_template["node"]
+#     services_dict = instance_template["services"]
+#
+#     # Fill service username
+#     data["service_username"] = username
+#
+#     # Instance info
+#     instance_data["cpu"] = data["num_nodes"] * data["cpu"]
+#     instance_data["mem"] = data["num_nodes"] * data["mem"]
+#     instance_data["number_of_disks"] = data["num_nodes"] * data["number_of_disks"]
+#     instance_data["number_of_nodes"] = data["num_nodes"]
+#     instance_data["instance_name"] = data["clustername"]
+#     instance_data["instance_state"] = "registered"
+#
+#     # Node info
+#     node_data["cpu"] = str(data["cpu"])  # You can remove str in the future when registry is fixed
+#     node_data["mem"] = str(data["mem"])  # You can remove str in the future when registry is fixed
+#     node_data["number_of_disks"] = str(data["number_of_disks"])
+#     node_data["status"] = "registered"
+#
+#     # nodes
+#     nodes_dict = dict()
+#     name_pattern = node_data["name"]
+#     for i in range(int(instance_data["number_of_nodes"])):
+#         copied_data = node_data.copy()
+#         copied_data["name"] = name_pattern+str(i)
+#         copied_data["node_id"] = str(i)
+#         copied_data["disks"] = ut.deflate_node_disks(node_data["number_of_disks"], node_data["disk"])
+#         copied_data.pop("disk", None)
+#         copied_data["networks"] = ut.deflate_node_networks(node_data["network"])
+#         copied_data.pop("network", None)
+#         nodes_dict[copied_data["name"]] = copied_data
+#
+#     instance_dn, instance_data = ut.register_instance(data, instance_data, nodes_dict, services_dict)
+#     app.logger.info("Cluster {} was successfully registered".format(instance_data["service_full_name"]))
+#
+#     for node in nodes_dict:
+#         node_dn = instance_dn + "/nodes/" + nodes_dict[node]["name"]
+#         ut.set_node_info(node_dn=node_dn, node_name=nodes_dict[node]["name"],
+#                          instance_name=instance_data["service_full_name"])
+#
+#     data = {"instance_dn": instance_dn}
+#     data_json = json.dumps(data)
+#     headers = {'Content-type': 'application/json'}
+#     response = requests.post(MESOS_FRAMEWORK_ENDPOINT, data=data_json, headers=headers)
+#     app.logger.info(
+#         "Cluster {} was successfully forwarded to the mesos framework".format(instance_data["service_full_name"]))
+#     app.logger.info(
+#         "Mesos framework response was {} ".format(response))
+#     return jsonify({'result': 'success'})
