9bc3c6ef95bd3b229b019dcd0d39aa9940528cf4
JonatanEnes <jonatan.enes@protonmail.com>
2016-06-27 01:13:51 +0200

    Fix de mis endpoints que uso para desarrollo, disculpas T_T

--- config/development.py
+++ config/development.py
@@ -8,8 +8,8 @@ SECRET_KEYS = '/etc/keyczar/keys'
 # Fill as needed
 DEBUG = True
 IGNORE_AUTH = True
-MESOS_FRAMEWORK_ENDPOINT = 'http://127.0.0.1:5004/bigdata/mesos_framework/v1/clusters'
-ORQUESTRATOR_ENDPOINT = 'http://127.0.0.1:5005/orquestrator/v1/clusters'
-#MESOS_FRAMEWORK_ENDPOINT = 'http://mesos_framework:5000/bigdata/mesos_framework/v1/clusters'
-#ORQUESTRATOR_ENDPOINT = 'http://orquestrator:5005/orquestrator/v1/clusters'
+#MESOS_FRAMEWORK_ENDPOINT = 'http://127.0.0.1:5004/bigdata/mesos_framework/v1/clusters'
+#ORQUESTRATOR_ENDPOINT = 'http://127.0.0.1:5005/orquestrator/v1/clusters'
+MESOS_FRAMEWORK_ENDPOINT = 'http://mesos_framework:5000/bigdata/mesos_framework/v1/clusters'
+ORQUESTRATOR_ENDPOINT = 'http://orquestrator:5005/orquestrator/v1/clusters'
 CONSUL_ENDPOINT = 'http://consul:8500/v1/kv'
