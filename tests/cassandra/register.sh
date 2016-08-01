a23c5042d50f19a562612fe4e9fb739f3d12e342
Javier Cacheiro <jlopez@cesga.es>
2016-08-01 11:58:29 +0200

    cassandra logo_url

--- tests/cassandra/register.sh
+++ tests/cassandra/register.sh
@@ -1,7 +1,7 @@
 # First get a TOKEN
 # export TOKEN='<TOKEN>'
 # export AUTH="x-auth-token: $TOKEN"
-http POST http://paas:6000/bigdata/api/v1/products name=cassandra version=3.0.8 description='The Cassandra database is the right choice when you need scalability and high availability without compromising performance.' x-auth-token:$TOKEN
+http POST http://paas:6000/bigdata/api/v1/products name=cassandra version=3.0.8 description='The Cassandra database is the right choice when you need scalability and high availability without compromising performance.' logo_url='http://hadoop.cesga.es/img/1280px-Cassandra_logo.svg.png' x-auth-token:$TOKEN
 curl -X PUT http://paas:6000/bigdata/api/v1/products/cassandra/3.0.8/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
 curl -X PUT http://paas:6000/bigdata/api/v1/products/cassandra/3.0.8/options --data-binary @options.json -H "$AUTH"
 curl -X PUT http://paas:6000/bigdata/api/v1/products/cassandra/3.0.8/orchestrator --data-binary @fabfile.py -H "$AUTH"
